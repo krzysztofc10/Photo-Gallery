01380354a1e84d87bbb2b350bcb43874dd28dc23
krzysztofc10 <krzysztofc10@gmail.com>
2021-11-14 18:00:50 +0100

    added functionality and styles to Detail view

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Grid } from '@material-ui/core';
 import { Master } from './Components/Master';
 import { Detail } from './Components/Detail';
 import './Css/App.css';
 
-function App() {
-  return (
-    <>
-      <div className="title">Photo Galery</div>
-      <Grid container spacing={2}>
-        <Grid item xs={4} className="gridCss">
-          <Master />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { url: '', dUrl: '' };
+    this.sendId = this.sendId.bind(this);
+  }
+
+  sendId(url, dUrl) {
+    this.setState({ url, dUrl });
+  }
+
+  render() {
+    const { url, dUrl } = this.state;
+
+    return (
+      <>
+        <div className="title">Photo Galery</div>
+        <Grid container spacing={2}>
+          <Grid item xs={4} className="gridCss">
+            <Master sendId={this.sendId} />
+          </Grid>
+          <Grid item xs={8}>
+            <Detail sendId={url} durl={dUrl} />
+          </Grid>
         </Grid>
-        <Grid item xs={8}>
-          <Detail />
-        </Grid>
-      </Grid>
-    </>
-  );
+      </>
+    );
+  }
 }
 
 export { App };
--- src/Components/Detail.jsx
+++ src/Components/Detail.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../Css/Detail.css';
 
-export function Detail() {
+export function Detail(props) {
+  const { sendId, durl } = props;
+
   return (
-    <div><p>Details</p></div>
+    <div>
+      { sendId !== ''
+     && (
+     <>
+       <img src={sendId} className="detailImage" alt="pic" />
+       <a className="button" href={`${durl}/download?force=true`} download>
+         Download photo
+       </a>
+     </>
+     )}
+    </div>
   );
 }
+
+Detail.propTypes = {
+  durl: PropTypes.string,
+  sendId: PropTypes.string,
+};
+
+Detail.defaultProps = {
+  durl: '',
+  sendId: '',
+};
